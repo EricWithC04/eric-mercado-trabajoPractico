8c7285c207552f13e21559e2079f11c4689bdbfb
EricWithC04 <negativegame117@gmail.com>
2023-08-25 14:54:33 -0300

    Initial Commit

---
+++ app.js
@@ -0,0 +1,10 @@
+import Express from "express";
+import dotenv from "dotenv";
+dotenv.config()
+const port = process.env.PORT
+
+const app = Express()
+
+app.listen(port, () => {
+    console.log(`Server listen on port ${port}`);
+})
