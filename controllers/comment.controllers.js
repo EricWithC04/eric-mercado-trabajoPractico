c50f241c52198beef8ba61a42b0aa50e6924a2ef
EricWithC04 <negativegame117@gmail.com>
2023-08-27 17:19:29 -0300

    feat: controladores para Comentarios listos

--- controllers/comment.controllers.js
+++ controllers/comment.controllers.js
@@ -17,3 +17,29 @@ export const getAllComments = async (req, res) => {
         res.status(err.status || 500).json(err.message || 'Error interno del servidor');
     }
 }
+
+export const createComment = async (req, res) => {
+    const { content } = req.body
+    try {
+        const today = new Date()
+        const newComment = await Comment.create({
+            content,
+            createDate: today.toISOString().split("T")[0]
+        })
+
+        if (!newComment) {
+            throw ({
+                status: 404,
+                message: 'No se ha podido crear el Comentario'
+            })
+        }
+
+        res.status(201).json({
+            message: 'Se ha creado el Comentario correctamente',
+            newComment
+        })
+    } catch (err) {
+        console.error(err);
+        res.status(err.status || 500).json(err.message || 'Error interno del servidor');
+    }
+}
