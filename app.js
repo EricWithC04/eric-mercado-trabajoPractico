9522e539aecd19b43a45fa835f4901c2cb30d7f5
EricWithC04 <negativegame117@gmail.com>
2023-08-28 09:24:44 -0300

    nada

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 import Express from "express";
 import dotenv from "dotenv";
-dotenv.config()
-const port = process.env.PORT
+dotenv.config();
+const port = process.env.PORT;
 
-const app = Express()
+const app = Express();
 
 app.listen(port, () => {
     console.log(`Server listen on port ${port}`);
-})
+});
